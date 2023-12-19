e5d9413a5e50717fe1ca8aed6009335122b3e0b9
Issa Alali <122840822+issaalali991@users.noreply.github.com>
2023-12-19 14:44:21 +0100

    Issa (#3)
    * handle filter for projects
    
    * handle scroll and submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState, useRef  } from 'react'
 import Header from './componants/1-header/Header'
 import Hero from './componants/2-hero/Hero'
 import Main from './componants/3-main/Main'
 import Contact from './componants/4-contact/Contact'
 import Footer from './componants/5-footer/Footer'
+import emailjs from '@emailjs/browser';
 
 function App() {
+  const [showscroll,setShowScroll]=useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+        if(window.scrollY>300){
+            setShowScroll(true);
+        }
+        else setShowScroll(false);
+    })
+  },[])
+  // const gotoUp= ()=>{
+  //   window.scrollY=0;
+  // }
 
+  // send message 
+  
   return (
     <div id='up' className='container'>
       <Header />
@@ -16,9 +32,11 @@ function App() {
       <Contact />
       <div className='divider' />
       <Footer />
-      <a href="#up">
-      <button className='scroll2top icon-keyboard_arrow_up'></button>
-      </a>
+     
+        <a href="#up" style={{opacity:showscroll ? 1 : 0, transition:'0.5s'}}>
+        <button className='scroll2top icon-keyboard_arrow_up'  ></button>
+        </a>
+    
     </div>
   )
 }
---
+++ src/componants/3-main/myProjects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    projectTitle: "react project",
+    category: ["react", "bootstrap"],
+    imgPath: "./project1.jpeg",
+  },
+  {
+    projectTitle: "CSS project",
+    category: ["html", "css"],
+    imgPath: "./project1.jpe",
+  },
+];
